b640221595268ec00492c2ded001bc5a78dfd99d
Nachiketh711 <kk6810@srmist.edu.in>
2026-03-02 08:47:56 +0530

    UC2 completed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,26 @@
 /*
-*@author NK
-*@version 1.0
+ *@author NK
+ *@Version 1.0
+ * This is to print Basic information
  */
+
 public class PalindromeCheckerApp {
-public static void main(String[] args){
-    System.out.println("welcome to the palindrome checker app");
-    System.out.println("version : 1.0");
-    System.out.println("System initialized successfully");
-}
-}
+    public static void main(String[] args) {
+//        System.out.println("PalindromeCheckerApp");
+//        System.out.println("Version : 1.0");
+//        System.out.println("System initialized successfully");
+        String input = "madam";
+        boolean isPalindrome = true;
 
+        // Loop till half of string
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
 
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome?: " + isPalindrome);
+    }
+}
